16b3b79065bf17933b0a7ed0f9c591246803f59a
Karteek <karteek.pericharla@gmail.com>
2025-05-15 14:57:19 -0500

    Set dynamic root paths for local hosting and hosting in render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/my-site/',
+  base: process.env.VITE_DEPLOY_TARGET === "gh-pages" ? "/my-site/" : "/",
   plugins: [react()],
 })
